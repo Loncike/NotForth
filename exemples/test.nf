fa0e7261376f6d6bc91078fb27c61b9e702df58c
Loncike <hdlive69@gmail.com>
2025-06-19 21:01:28 +0200

    Immediate number, tokenizer implamented

---
+++ exemples/test.nf
@@ -0,0 +1,3 @@
+store 0 $55
+add 0 $14 0
+print 0
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import operator, sys
+from dataclasses import dataclass
+from enum import Enum
 
 logicOps = {
     "==": operator.eq,
@@ -9,46 +11,70 @@ logicOps = {
     ">=": operator.ge,
 }
 
+class TokenType(Enum):
+    NUMBER = 1
+    INDEX = 2
+    OP = 3
+    LOGICOP = 4
+    LABEL = 5
+
+@dataclass
+class Token:
+    type: TokenType
+    data: int | None | str
+
 Mem = []
 
 def CheckMem(idx):
-    if len(Mem)-1 < int(idx):
-        for i in range((int(idx) - len(Mem)) + 1):
-            Mem.append(0)
+    try:
+        if len(Mem)-1 < int(idx):
+            for i in range((int(idx) - len(Mem)) + 1):
+                Mem.append(0)
+    except:
+        pass
 
-def Add(idx1, idx2, idx3):
-    Mem[int(idx3)] = Mem[int(idx1)] + Mem[int(idx2)]
+def Add(t1, t2, t3):
+    n1 = Mem[t1.data] if t1.type == TokenType.INDEX else t1.data
+    n2 = Mem[t2.data] if t2.type == TokenType.INDEX else t2.data
+    Mem[t3.data] = n1 + n2
 
-def Sub(idx1, idx2, idx3):
-    Mem[int(idx3)] = Mem[int(idx1)] - Mem[int(idx2)]
+def Sub(t1, t2, t3):
+    n1 = Mem[t1.data] if t1.type == TokenType.INDEX else t1.data
+    n2 = Mem[t2.data] if t2.type == TokenType.INDEX else t2.data
+    Mem[t3.data] = n1 + n2
 
-def Store(idx, data):
-    Mem[int(idx)] = int(data)
+def Store(t1, t2):
+    Mem[t1.data] = Mem[t2.data] if t2.type == TokenType.INDEX else t2.data
 
 def JumpIf(idx1, op, idx2) -> bool:
     return logicOps[op](Mem[int(idx1)], Mem[int(idx2)])
 
-def runProgram(program, labels):
+def runProgram(tokens, labels):
 
     head = 0
-    while head < len(program):
-        p = program[head].lower().split(" ")
-        if p[0] == "store":
-            CheckMem(p[1])
-            Store(p[1], p[2])
-        elif p[0] == "add":
-            CheckMem(p[1])
-            Add(p[1], p[2], p[3])
-        elif p[0] == "sub":
-            CheckMem(p[1])
-            Sub(p[1], p[2], p[3])
-        elif p[0] == "jumpif":
-            if JumpIf(p[1], p[2], p[3]): 
-                head = labels[p[4]]
+    while head < len(tokens):
+        if tokens[head].type != TokenType.OP: 
+            exit(1)
+        if tokens[head].data == "store":
+            CheckMem(tokens[head+1].data)
+            if tokens[head+2].type == TokenType.INDEX: CheckMem(tokens[head+2].data)
+            Store(tokens[head+1], tokens[head+2])
+            head+=2
+        elif tokens[head].data == "add":
+            if tokens[head+3].type == TokenType.INDEX: CheckMem(tokens[head+3].data)
+            Add(tokens[head+1], tokens[head+2], tokens[head+3])
+            head+=3
+        elif tokens[head].data == "sub":
+            if tokens[head+3].type == TokenType.INDEX: CheckMem(tokens[head+3].data)
+            Sub(tokens[head+1], tokens[head+2], tokens[head+3])
+            head+=3
+        elif tokens[head].data == "jumpif":
+            if JumpIf(tokens[head+1], tokens[head+2], tokens[head+3]): 
+                head = labels[tokens[head+4]]
                 continue
-        elif p[0] == "print":
-            print(Mem[int(p[1])])
-
+        elif tokens[head].data == "print":
+            print(Mem[tokens[head+1].data])
+            head+=1
         head+=1
 
 def loadProgram(file) -> str:
@@ -63,6 +89,65 @@ def generateLabels(program):
             labels[p[1]] = i
     return labels
 
+
+
+
+def GenerateTokens(program) -> list:
+    tokens = []
+    for p in program:
+        p = p.lower().split(" ")
+        if p[0] == "store":
+            tokens.append(Token(TokenType.OP, "store"))
+            tokens.append(Token(TokenType.INDEX, int(p[1])))
+            if p[2][0] == "$": 
+                tokens.append(Token(TokenType.NUMBER, int(p[2][1:])))
+            else:
+                tokens.append(Token(TokenType.INDEX, int(p[2])))
+
+        elif p[0] == "add":
+            tokens.append(Token(TokenType.OP, "add"))
+            if p[1][0] == "$": 
+                tokens.append(Token(TokenType.NUMBER, int(p[1][1:])))
+            else:
+                tokens.append(Token(TokenType.INDEX, int(p[1])))
+            if p[2][0] == "$": 
+                tokens.append(Token(TokenType.NUMBER, int(p[2][1:])))
+            else:
+                tokens.append(Token(TokenType.INDEX, int(p[2])))
+            tokens.append(Token(TokenType.INDEX, int(p[3])))
+
+        elif p[0] == "sub":
+            tokens.append(Token(TokenType.OP, "sub"))
+            if p[1][0] == "$": 
+                tokens.append(Token(TokenType.NUMBER, int(p[1][1:])))
+            else:
+                tokens.append(Token(TokenType.INDEX, int(p[1])))
+            if p[2][0] == "$": 
+                tokens.append(Token(TokenType.NUMBER, int(p[2][1:])))
+            else:
+                tokens.append(Token(TokenType.INDEX, int(p[2])))
+            tokens.append(Token(TokenType.INDEX, int(p[3])))
+
+        elif p[0] == "jumpif":
+            tokens.append(Token(TokenType.OP, "jumpif"))
+            if p[1][0] == "$": 
+                tokens.append(Token(TokenType.NUMBER, int(p[1][1:])))
+            else:
+                tokens.append(Token(TokenType.INDEX, int(p[1])))     
+            tokens.append(Token(TokenType.LOGICOP, p[2]))
+            if p[3][0] == "$": 
+                tokens.append(Token(TokenType.NUMBER, int(p[3][1:])))
+            else:
+                tokens.append(Token(TokenType.INDEX, int(p[3])))
+            tokens.append(Token(TokenType.LABEL, p[4]))
+        elif p[0] == "print":
+            tokens.append(Token(TokenType.OP, "print"))
+            tokens.append(Token(TokenType.INDEX, int(p[1])))
+    return tokens
+
+
+
 if __name__ == "__main__":
     program = loadProgram(sys.argv[1])
-    runProgram(program, generateLabels(program))
+    runProgram(GenerateTokens(program), generateLabels(program))
+
