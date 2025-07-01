c6a1bf82ed8803bf3a635988d12cc43cace8ab44
Loncike <hdlive69@gmail.com>
2025-07-01 18:41:55 +0200

    Introduced a compiler, added keyword printnum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from string import ascii_letters, digits
-import operator, sys
+import operator, sys, os, subprocess
 import ctypes, ctypes.util
 from dataclasses import dataclass
 from enum import Enum, auto
@@ -33,7 +33,7 @@ class Macro:
     tokens: list
     argumentsIdx: dict
 
-builtinwords = ["store", "add", "sub", "jumpif", "jump", "syscallargs", "syscall", "macro", "include", "end", "label"]
+builtinwords = ["store", "add", "sub", "jumpif", "jump", "syscallargs", "syscall", "macro", "include", "end", "label", "printnum"]
 
 def lexFile(filePath):
     with open(filePath, "r", encoding="utf-8") as f:
@@ -142,7 +142,6 @@ def preProcess(tokens, macros={}):
             replacedTokens = []
             for t in macro.tokens:
                 if t.type == TokenType.WORD and t.value in macro.argumentsIdx:
-                    print(t)
                     replacedTokens.append(args[macro.argumentsIdx[t.value]])
                 else:
                     replacedTokens.append(t)
@@ -380,13 +379,217 @@ class Interpreter():
                         ip+=1
                     ip-=1
                     self.doSyscall(syscallNum, args)
+            
+            elif self.Program[ip].type == TokenType.BUILTINWORD and self.Program[ip].value == "printnum":
+                    t1 = self.Program[ip+1]
+                    n1 = 0
+                    if t1.type == TokenType.INDEX:
+                        n1 = self.Mem[t1.value] 
+                    elif t1.type == TokenType.POINTER:
+                        n1 = self.Mem[self.Mem[t1.value]]
+                    else: 
+                        n1 = t1.value
+
+                    print(int(self.Mem[n1]))
+                    ip+=1
 
             ip+=1
         print("Memory dump: ", self.Mem) if debug else None
 
 
+class Compiler():
+    def __init__(self, p, l, out):
+        self.Program = p
+        self.Labels = l
+        self.SYSCALLARGUMENTSREGISTERS = ["edi", "esi", "edx", "r10d", "r8d", "r9d"]
+        self.SYSCALLS = {
+                0: ["uint", "mem_out", "size_t"],
+                1: ["uint", "mem_in", "size_t"],
+                60: ["int"]
+
+                }
+        self.outName = "./bin/"+out
+
+    def Run(self, debug=False):
+        ip = 0
+        file = open(self.outName+".asm", "w", encoding="utf-8")
+        file.write("format ELF64 executable 3\nentry main\nsegment readable writeable\n    mem dd 1024 dup(0)\nsegment readable executable\nprintnum:\n    mov     r9, -3689348814741910323\n    sub     rsp, 40\n    mov     byte [rsp+31], 10\n    lea     rcx, [rsp+30]\n.L2:\n    mov     rax, rdi\n    and     eax, 0xFFFFFFFF           ; mask input to 32 bits\n    lea     r8, [rsp+32]\n    mul     r9\n    mov     rax, rdi\n    and     eax, 0xFFFFFFFF           ; mask input again\n    sub     r8, rcx\n    shr     rdx, 3\n    lea     rsi, [rdx+rdx*4]\n    add     rsi, rsi\n    sub     rax, rsi\n    add     eax, 48\n    mov     byte [rcx], al\n    mov     rax, rdi\n    and     eax, 0xFFFFFFFF\n    mov     rdi, rdx\n    mov     rdx, rcx\n    sub     rcx, 1\n    cmp     rax, 9\n    ja      .L2\n    lea     rax, [rsp+32]\n    mov     edi, 1\n    sub     rdx, rax\n    xor     eax, eax\n    lea     rsi, [rsp+32+rdx]\n    mov     rdx, r8\n    mov     rax, 1\n    syscall\n    add     rsp, 40\n    ret\nmain:\n")
+        while ip < len(self.Program):
+            for k, v in  self.Labels.items():
+                if v-1 == ip:
+                    file.write(f"{k}:\n")
+
+            if self.Program[ip].type == TokenType.BUILTINWORD and self.Program[ip].value == "store":
+                t1 = self.Program[ip+1]
+                t2 = self.Program[ip+2]
+                if t2.type == TokenType.INDEX:
+                    file.write(f"    mov eax, dword [mem + {t2.value}*4]\n")
+                    file.write(f"    mov dword [mem + {t1.value}*4], eax\n")
+                else: 
+                    file.write(f"    mov dword [mem + {t1.value}*4], {t2.value}\n")
+                ip+=2
+
+            elif self.Program[ip].type == TokenType.BUILTINWORD and self.Program[ip].value == "add":
+                t1 = self.Program[ip+1]
+                t2 = self.Program[ip+2]
+                t3 = self.Program[ip+3]
+                if t1.type == TokenType.INDEX:
+                    file.write(f"    mov eax, dword [mem + {t1.value}*4]\n")
+                elif t1.type == TokenType.POINTER:
+                    file.write(f"    mov eax, dword [mem + {t1.value}*4]\n")
+                    file.write(f"    mov eax, dword [mem + eax*4]\n")
+                else: 
+                    file.write(f"    mov eax, {t1.value}")
+
+                if t2.type == TokenType.INDEX:
+                    file.write(f"    mov ebx, dword [mem + {t2.value}*4]\n")
+                elif t2.type == TokenType.POINTER:
+                    file.write(f"    mov ebx, dword [mem + {t2.value}*4]\n")
+                    file.write(f"    mov ebx, dword [mem + ebx*4]\n")
+                else: 
+                    file.write(f"    mov ebx, {t2.value}\n")
+
+                file.write(f"    add eax, ebx\n")
+
+                
+                if t3.type == TokenType.INDEX:
+                    file.write(f"    mov dword [mem + {t3.value}*4], eax\n")
+                elif t3.type == TokenType.POINTER:
+                    file.write(f"    mov ebx, dword [mem + {t3.value}*4]\n")
+                    file.write(f"    mov dword [mem + ebx*4], eax\n")
+                ip+=3
+
+            elif self.Program[ip].type == TokenType.BUILTINWORD and self.Program[ip].value == "sub":
+                t1 = self.Program[ip+1]
+                t2 = self.Program[ip+2]
+                t3 = self.Program[ip+3]
+                if t1.type == TokenType.INDEX:
+                    file.write(f"    mov eax, dword [mem + {t1.value}*4]\n")
+                elif t1.type == TokenType.POINTER:
+                    file.write(f"    mov eax, dword [mem + {t1.value}*4]\n")
+                    file.write(f"    mov eax, dword [mem + eax*4]\n")
+                else: 
+                    file.write(f"    mov eax, {t1.value}\n")
+
+                if t2.type == TokenType.INDEX:
+                    file.write(f"    mov ebx, dword [mem + {t2.value}*4]\n")
+                elif t2.type == TokenType.POINTER:
+                    file.write(f"    mov ebx, dword [mem + {t2.value}*4]\n")
+                    file.write(f"    mov ebx, dword [mem + ebx*4]\n")
+                else: 
+                    file.write(f"    mov ebx, {t2.value}\n")
+
+                file.write(f"    sub eax, ebx\n")
+
+                
+                if t3.type == TokenType.INDEX:
+                    file.write(f"    mov dword [mem + {t3.value}*4], eax\n")
+                elif t3.type == TokenType.POINTER:
+                    file.write(f"    mov ebx, dword [mem + {t3.value}*4]\n")
+                    file.write(f"    mov dword [mem + ebx*4], eax\n")
+
+                ip+=3
+            elif self.Program[ip].type == TokenType.BUILTINWORD and self.Program[ip].value == "jumpif":
+                t1 = self.Program[ip+1]
+                op = self.Program[ip+2].value
+                t3 = self.Program[ip+3]
+                if t1.type == TokenType.INDEX:
+                    file.write(f"    mov eax, dword [mem + {t1.value}*4]\n")
+                elif t1.type == TokenType.POINTER:
+                    file.write(f"    mov eax, dword [mem + {t1.value}*4]\n")
+                    file.write(f"    mov eax, dword [mem + eax*4]\n")
+                else: 
+                    file.write(f"    mov eax, {t1.value}\n")
+
+                if t3.type == TokenType.INDEX:
+                    file.write(f"    mov ebx, dword [mem + {t3.value}*4]\n")
+                elif t3.type == TokenType.POINTER:
+                    file.write(f"    mov ebx, dword [mem + {t3.value}*4]\n")
+                    file.write(f"    mov ebx, dword [mem + ebx*4]\n")
+                else: 
+                    file.write(f"    mov ebx, {t2.value}\n")
+
+                file.write(f"    cmp eax, ebx\n")
+                
+                if op == "==":
+                    file.write(f"    je {self.Program[ip+4].value}\n")
+                elif op == "!=":
+                    file.write(f"    jne {self.Program[ip+4].value}\n")
+                elif op == "<":
+                    file.write(f"    jl {self.Program[ip+4].value}\n") ## for no less than (signed)
+                elif op == "<=":
+                    file.write(f"    jle {self.Program[ip+4].value}\n")
+                elif op == ">":
+                    file.write(f"    jg {self.Program[ip+4].value}\n")
+                elif op == ">=":
+                    file.write(f"    jge {self.Program[ip+4].value}\n")
+                ip+=4
+
+            elif self.Program[ip].type == TokenType.BUILTINWORD and self.Program[ip].value == "jump":
+                    file.write(f"    jmp {self.Program[ip+1].value}\n")
+                    ip+=1
+            elif self.Program[ip].type == TokenType.BUILTINWORD and self.Program[ip].value == "syscall":
+                    ip+=1
+                    syscallNum = self.Program[ip].value
+                    file.write(f"    mov eax, {syscallNum}\n")
+                    ip+=1
+                    args = []
+                    while ip < len(self.Program) and self.Program[ip].type in [TokenType.INDEX, TokenType.NUMBER, TokenType.POINTER]:
+                        args.append(self.Program[ip])
+                        ip+=1
+                    ip-=1
+                    for i in range(len(args)):
+                        if args[i].type == TokenType.INDEX:
+                            file.write(f"    lea {self.SYSCALLARGUMENTSREGISTERS[i]}, [mem + {args[i].value}*4]\n")
+                        elif args[i].type == TokenType.POINTER:
+                            file.write(f"    mov {self.SYSCALLARGUMENTSREGISTERS[i]}, dword [mem + {args[i].value}*4]\n")
+                            file.write(f"    lea {self.SYSCALLARGUMENTSREGISTERS[i]}, dword [mem + {self.SYSCALLARGUMENTSREGISTERS[i]}*4]\n")
+                        else: 
+                            if self.SYSCALLS[syscallNum][i] in ["uint", "int"]:
+                                file.write(f"    mov {self.SYSCALLARGUMENTSREGISTERS[i]}, {args[i].value}\n")
+                            elif self.SYSCALLS[syscallNum][i] == "size_t":
+                                file.write(f"    mov {self.SYSCALLARGUMENTSREGISTERS[i]}, {args[i].value}*4\n")
+
+                    file.write(f"    syscall\n")
+
+            elif self.Program[ip].type == TokenType.BUILTINWORD and self.Program[ip].value == "printnum":
+                t1 = self.Program[ip+1]
+                if t1.type == TokenType.INDEX:
+                    file.write(f"    mov edi, dword [mem + {t1.value}*4]\n")
+                elif t1.type == TokenType.POINTER:
+                    file.write(f"    mov eax, dword [mem + {t1.value}*4]\n")
+                    file.write(f"    mov edi, dword [mem + eax*4]\n")
+                else: 
+                    file.write(f"    mov edi, {t1.value}\n")
+                
+                file.write(f"    call printnum\n")
+                ip+=1
+
+            ip+=1
+        file.write("    mov rax, 60\n    syscall") 
+
 
 if __name__ == "__main__":
-    program, labels = preProcess(lexFile(sys.argv[1]))
-    inter = Interpreter(program, labels)
-    inter.Run(False)
+    mode = sys.argv[1]
+    if mode == "help":
+        print("Compile: python main.py com <dest to file> | Compiled elf64 file will be in ./bin")
+        print("Interpret: python main.py int <dest to file> <*debug> | Debug is an optionnal flag")
+        exit(0)
+    program, labels = preProcess(lexFile(sys.argv[2]))
+    if mode == "int":
+        inter = Interpreter(program, labels)
+        if len(sys.argv) > 3 and sys.argv[3] == "debug":
+            inter.Run(True)
+        else:
+            inter.Run(False)
+    elif mode == "com":
+        if not os.path.exists("./bin"):
+            os.makedirs("./bin")
+        fileName = sys.argv[2].split("/")[-1][:-3]
+        comp = Compiler(program, labels, fileName)
+        comp.Run()
+        result = subprocess.run(["fasm", "./bin/"+fileName+".asm"], capture_output=True, text=True)
+        if result.returncode != 0:
+            print("ERROR:", result.stderr)
+        os.remove("./bin/"+fileName+".asm")
+    
